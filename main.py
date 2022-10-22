8310ed87fc5f6c31ccb6e9ac46ccaf4b5853aae2
Mateusz Monikowski <mateuszmonikowski@gmail.com>
2022-10-22 13:20:13 +0200

    dziala

--- main.py
+++ main.py
@@ -12,15 +12,21 @@ if __name__ == '__main__':
         tokens = nltk.TreebankWordTokenizer().tokenize(lines)
         spans = list(nltk.TreebankWordTokenizer().span_tokenize(lines))
 
-        # foundWords = lib.get_words()
-        #
-        # for token in tokens:
-        #     index = lib.is_word(token)
-        #     if index != -1:
+        word_list = lib.get_words()
 
-        print(lib.get_words())
-        print(lib.is_word('Senator'))
-        print(tokens)
-        print(spans)
+        words_positions = {}
+        token_spans = list(zip(tokens, spans))
+
+        for token_span in token_spans:
+            token_word = token_span[0]
+            start_idx = token_span[1][0]
+            if lib.is_word(token_word) > -1:
+                if token_word not in words_positions:
+                    words_positions[token_word] = [start_idx]
+                else:
+                    words_positions[token_word].append(start_idx)
+
+        print(words_positions.items())
+
 
 
