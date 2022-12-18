f050586f9669754e60cf5a44fa52b53f7b1b0084
Mateusz Monikowski <mateuszmonikowski@gmail.com>
2022-12-18 14:34:37 +0100

    add short texts and their evaluation data

---
+++ NZ_Eval/text4/short_text4_evaluation_data.py
@@ -0,0 +1,8 @@
+short_text4_evaluation_data = [
+    ('bay of islands', [(99, 113)]),
+    ('lake taupo', [(227, 237), (348, 358)]),
+    ('tongariro national park', [(246, 269), (666, 689)]),
+    ('north island', [(364, 378), (525, 537)]),
+    ('new zealand', [(405, 416)]),
+    ('taupo', [(450, 455), (634, 639)])
+]
